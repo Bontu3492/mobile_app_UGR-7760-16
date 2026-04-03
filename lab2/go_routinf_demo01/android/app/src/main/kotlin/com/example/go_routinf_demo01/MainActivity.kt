a3f8d628d866a26519b3e8f0a2fb9d8ebb55d5ad
Tinsu Jembere <tinsaejembere4@gmail.com>
2026-04-03 22:50:02 +0300

    adding the assignment

---
+++ lab2/go_routinf_demo01/android/app/src/main/kotlin/com/example/go_routinf_demo01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.go_routinf_demo01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lab2/go_routinf_demo01/lib/main.dart
@@ -0,0 +1,47 @@
+ // lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() => runApp(const MyApp());
+
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    GoRoute(path: '/second', builder: (context, state) => const SecondScreen()),
+  ],
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(routerConfig: _router, title: 'GoRouter Demo 01');
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go('/second'),
+          child: const Text('Go to Second Screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second')),
+      body: const Center(child: Text('Second Screen')),
+    );
+  }
+}
